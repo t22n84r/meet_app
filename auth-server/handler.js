75df8da6723321f6da055406e61c0c7ef7da0115
t22n84r <81875062+t22n84r@users.noreply.github.com>
2023-09-09 11:35:21 -0400

    Full-Stack Immersion Achievement-4 Exercise-4.3
    https://careerfoundry.com/en/course/full-stack-immersion/exercise/writing-testing-serverless-functions#task
    
    Updated serverless.yml with 3 new functions
    
    Updated handler.js with code to implement the 3 new functions
    
    Added googleapis package for authentication to auth-server folder package.json
    
    Updated .gitignore with new file & folder to ignore

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,115 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const { google } = require('googleapis');
+const calendar = google.calendar({ version: 'v3' });
+const scopes = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
+const { CLIENT_ID, CLIENT_SECRET, CALENDAR_ID } = process.env;
+const redirect_uris = ['https://t22n84r.github.io/meet_app/'];
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, redirect_uris[0]);
+
+module.exports.getAuthURL = async (event) => {
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+    prompt: 'consent',
+  });
+
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    },
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+        authUrl,
+    }),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.getAccessToken = async (event) => {
+  try {
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+    // Exchange the authorization code for an access token
+    const { tokens } = await oAuth2Client.getToken(code);
+
+    // Set the credentials to the OAuth2 client
+    oAuth2Client.setCredentials(tokens);
+
+    // Respond with the tokens
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      },
+      body: JSON.stringify(tokens),
+    };
+  } catch (error) {
+    console.error('An error occurred:', error);
+
+    // Respond with the error
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      },
+      body: JSON.stringify(error),
+    };
+  }
+};
+
+module.exports.getCalendarEvents = async (event) => {
+
+  try {
+
+    const access_token = event.pathParameters.access_token;
+    oAuth2Client.setCredentials({access_token: access_token});
+    const events = await  calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      }
+    );
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      },
+      body: JSON.stringify(events),
+    };
+  } catch (error) {
+
+    console.error('An error occurred:', error);
+
+    // Respond with the error
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      },
+      body: JSON.stringify(error),
+    };
+  }
 };
